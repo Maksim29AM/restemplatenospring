0359bd12b03d9b5c1a72c55b12329dcb19998ba5
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-06-06 12:23:01 +0200

    clean java vs spring

---
+++ src/main/java/org/example/myresttemplate/RestTemplate.java
@@ -0,0 +1,12 @@
+package org.example.myresttemplate;
+
+import java.net.http.HttpClient;
+
+public class RestTemplate {
+
+    public String exchange() {
+        // make HTTP request
+
+        return "made request";
+    }
+}
